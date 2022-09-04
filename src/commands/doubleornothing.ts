f5225bb9dd6446ba2e2f080c594f182e7fdb810c
Wes <averagewizard13@gmail.com>
2022-09-03 19:29:04 -0700

    Add double or nothing command

---
+++ src/commands/doubleornothing.ts
@@ -0,0 +1,37 @@
+import {
+  AttachmentBuilder,
+  CommandInteraction,
+  SlashCommandBuilder,
+} from "discord.js";
+import path from "path";
+import * as utils from "../utils";
+
+export const builder = new SlashCommandBuilder()
+  .setName("doubleornothing")
+  .setDescription(
+    "50% chance to double your points, 50% chance to lose your points"
+  );
+
+export const execute = async (interaction: CommandInteraction) => {
+  const user = await utils.fetchCreateUser(interaction.user.id);
+
+  if (Math.random() < 0.5) {
+    // -- Double
+    const oldPoints = user.sockpoints;
+    user.sockpoints *= 2;
+    await user.save();
+
+    await interaction.reply(
+      `<@${interaction.user.id}> DOUBLED BABY!! ${oldPoints} -> ${user.sockpoints}`
+    );
+  } else {
+    // -- Nothing
+    user.sockpoints = 0;
+    await user.save();
+
+    const attachment = new AttachmentBuilder(
+      path.join(__dirname, "../images/cry-man.gif")
+    );
+    await interaction.reply({ files: [attachment] });
+  }
+};
