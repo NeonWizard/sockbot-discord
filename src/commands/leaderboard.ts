cc4368bb70d7c25f2f44a9e273413fa7c54fce37
Wes <averagewizard13@gmail.com>
2022-10-01 01:55:43 -0700

    Change leaderboard command reply to embed

--- src/commands/leaderboard.ts
+++ src/commands/leaderboard.ts
@@ -1,7 +1,9 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { Bot } from "../bot";
 import { User } from "../database/models/User";
 import { BotCommand } from "../interfaces";
+
+const AWARD_LEVELS = ["🥇", "🥈", "🥉", "😏", "🤡"];
 
 export const LeaderboardCommand: BotCommand = {
   // prettier-ignore
@@ -10,27 +12,32 @@ export const LeaderboardCommand: BotCommand = {
     .setDescription("it IS a competition"),
 
   execute: async (bot: Bot, interaction: CommandInteraction) => {
-    // prettier-ignore
     const topUsers = await User.createQueryBuilder("leaderboard")
       .select()
-      .addSelect("sockpoints + \"bankBalance\"", "total_points")
+      .addSelect('sockpoints + "bankBalance"', "total_points")
       .orderBy("total_points", "DESC")
       .limit(5)
       .getMany();
 
-    // dont look at me
-    interaction.reply({
-      content:
-        "Top 5 richest people\n\n" +
-        topUsers
-          .map(
-            (x, i) =>
-              `${i + 1}. <@${x.discordID}>\n${(
-                x.sockpoints + x.bankBalance
-              ).toLocaleString()} sockpoints`
-          )
-          .join("\n\n"),
-      allowedMentions: { users: [] },
-    });
+    const embed = new EmbedBuilder()
+      .setColor(0x8a51ff)
+      .setTitle("Leaderboard")
+      .setDescription("The top 5 sockheads in the sockonomy")
+      .setThumbnail("https://i.imgur.com/65ZUH7y.png")
+      .setFields(
+        await Promise.all(
+          topUsers.map(async (user, i) => {
+            const discordUser = await bot.client.users.fetch(user.discordID);
+            return {
+              name: `${AWARD_LEVELS[i]} ${discordUser.tag}`,
+              value: `${(user.sockpoints + user.bankBalance).toLocaleString()} sockpoints`,
+            };
+          })
+        )
+      )
+      .setFooter({ text: "get superior <3" })
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [embed] });
   },
 };
