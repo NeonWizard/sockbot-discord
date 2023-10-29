69c10d78d4500cf0827fc28be3cfc8aecd41d532
Wes <averagewizard13@gmail.com>
2023-10-29 11:59:55 -0700

    (WIP) Command calling and info fetching through GPT

--- src/modules/openai.ts
+++ src/modules/openai.ts
@@ -5,11 +5,42 @@ import { OPENAI_COOLDOWN } from "../constants";
 import OpenAI from "openai";
 import { ChatCompletionMessageParam } from "openai/resources";
 
-let cooldown = 0;
+const CONTEXT_PROMPT = `
+The following is a conversation between {username} and multiple other users. {username} types in all lowercase, not using punctuation frequently. His grammar is eccentric and he has a weird personality. He does not send emojis, and tries not to be too excessively friendly. {username}'s intelligence is very low and reflects on how he talks. {username} is NOT an AI assistant, and no reference to that nor GPT should be made during the conversation.
+
+Use the following data as context as needed. It is provided in tabular format.
+
+--------------------
+User information:
+username | points | bank balance | double or nothing wins | double or nothing losses
+--- | --- | --- | --- | ---
+catsock | 64,359 | 110 | 7 | 13
+markiplier | 6459 | 0 | 0 | 0
+meemo | 15 | 15 | 1 | 0
+catsock employee | 688,123 | 100,001 | 50 | 51
+--------------------
+
+Additionally, a user may request to perform an action. If you are certain a user has the intent of executing one of these commands, {username} should respond with [<COMMAND> <arg1> <arg2> <...>] instead of a normal response, where <COMMAND> is the exact name of the command to be ran and zero or more arguments are included after. No other text should be included outside of the command. Following this, {username} will be provided with the output of executing the command, and should respond to the original query using that output.
+
+Listed below are the available commands, each with a description of what it does.
+
+Commands:
+- doubleornothing: Has a 50% chance of either doubling the user's points, or losing all of their points.
+- points: Displays how many points a user has.
+- bank balance: Displays how many points a user has stored in their bank.
+- pay <target username> <amount>: Pays the target person an amount of points from their own balance.
+`;
+
+// Builds a context for a message from the previous {limit} messages.
+const buildContext = (message: Message, limit = 30): ChatCompletionMessageParam[] => {
+  const messages = message.channel.messages.fetch({ before: message.id, limit: limit });
+  return [];
+};
 
 export default (bot: Bot): void => {
   const client = bot.client;
   let openAI: OpenAI;
+  let cooldown = 0;
 
   client.on("initialized", async () => {
     if (process.env.OPENAI_DISABLE === "true") {
@@ -46,6 +77,7 @@ export default (bot: Bot): void => {
 
     if (message.type === MessageType.Default) {
       if (!message.content.toLowerCase().includes(client.user.username.toLowerCase())) return;
+      context.unshift(...buildContext(message));
     } else if (message.type === MessageType.Reply) {
       let repliedTo = await message.fetchReference();
       if (repliedTo.author.id !== client.user.id) return;
@@ -78,7 +110,7 @@ export default (bot: Bot): void => {
 
     // -- pre-processing input
     const contextPrompt: ChatCompletionMessageParam = {
-      content: `The following is a conversation between ${client.user.username.toLowerCase()} and multiple other users. ${client.user.username.toLowerCase()} types in all lowercase, not using punctuation frequently. His grammar is eccentric and he has a weird personality. He does not send emojis, and tries not to be too excessively friendly. ${client.user.username.toLowerCase()}'s intelligence is very low and reflects on how he talks. ${client.user.username.toLowerCase()} is NOT an AI assistant, and no reference to that nor GPT should be made during the conversation.`,
+      content: CONTEXT_PROMPT.replaceAll("{username}", client.user.username.toLowerCase()),
       role: "system",
     };
     context.unshift(contextPrompt);
